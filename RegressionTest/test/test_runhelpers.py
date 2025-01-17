4961d4a322d110e57c65331bbc1b4f14028b8d11
Petros Koutsolampros <commits@orange-vertex.net>
2025-01-17 18:03:25 +0200

    Switch to checking for the start of the error

--- RegressionTest/test/test_runhelpers.py
+++ RegressionTest/test/test_runhelpers.py
@@ -63,10 +63,7 @@ class TestRunHelpers(unittest.TestCase):
             runhelpers.prepareDirectory(d.name())
             retcode, output = runhelpers.runExecutable( d.name(), [sys.executable, "-c", "raise Exception()"])
             self.assertFalse(retcode)
-            if sys.version_info[0] >= 3.13:
-                self.assertEqual(output, 'Traceback (most recent call last):\n  File "<string>", line 1, in <module>\nException\n')
-            else:
-                self.assertEqual(output, 'Traceback (most recent call last):\n  File "<string>", line 1, in <module>\n    raise Exception()\nException\n')
-            
+            self.assertTrue(output.startsWith('Traceback (most recent call last):\n  File "<string>", line 1, in <module>'))
+
 if __name__=="__main__":
     unittest.main()
