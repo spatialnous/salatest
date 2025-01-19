5e958b4e52d0aa0b48ea3be647548e04fd50cec9
Petros Koutsolampros <commits@orange-vertex.net>
2025-01-19 17:32:42 +0200

    Fix known result test runner

--- RegressionTest/knownresulttestrunner.py
+++ RegressionTest/knownresulttestrunner.py
@@ -42,7 +42,7 @@ class KnownResultTestRunner(depthmaprunner.DepthmapRegressionRunner):
         testDir, testOutFile, message = self.runTestCaseTest(name, cmds, extraArgs["test"])
 
         if testOutFile is None:
-            return (False, "Run {0} failed with message: {1}".format(i, message))
+            return (False, "Run failed with message: {1}".format(message))
 
         if testOutFile.endswith(".graph"):
             return (False, "Can not run with graph files ({0})".format(result))
