edfa10819763bb9359b7739aa9a74072c59c0dd1
Petros Koutsolampros <commits@orange-vertex.net>
2025-01-20 00:40:59 +0200

    Run all regression tests before failing

--- ci/runTests.sh
+++ ci/runTests.sh
@@ -13,21 +13,29 @@ cd ..
 ln -s build build-RegressionTest
 cd RegressionTest/test && echo pwd && python3 -u test_main.py || exit 1
 cd .. && pwd
+
+regression_error=0
+
 echo running known result tests
-python3 -u RegressionTestRunner.py knownresulttests_pointmap.json || exit 1
-python3 -u RegressionTestRunner.py knownresulttests_axial.json || exit 1
-python3 -u RegressionTestRunner.py knownresulttests_segment.json || exit 1
-python3 -u RegressionTestRunner.py knownresulttests_isovist.json || exit 1
-python3 -u RegressionTestRunner.py knownresulttests_conversion.json || exit 1
-python3 -u RegressionTestRunner.py knownresulttests_agents.json || exit 1
+python3 -u RegressionTestRunner.py knownresulttests_pointmap.json || regression_error=1
+python3 -u RegressionTestRunner.py knownresulttests_axial.json || regression_error=1
+python3 -u RegressionTestRunner.py knownresulttests_segment.json || regression_error=1
+python3 -u RegressionTestRunner.py knownresulttests_isovist.json || regression_error=1
+python3 -u RegressionTestRunner.py knownresulttests_conversion.json || regression_error=1
+python3 -u RegressionTestRunner.py knownresulttests_agents.json || regression_error=1
 echo running standard regression tests
-python3 -u RegressionTestRunner.py || exit 1
+python3 -u RegressionTestRunner.py || regression_error=1
 echo running agent test
-python3 -u RegressionTestRunner.py regressionconfig_agents.json || exit 1
+python3 -u RegressionTestRunner.py regressionconfig_agents.json || regression_error=1
 # search the modules directory for regression tests and run them
 for subdir in ../modules/*/; do
     regressionFile="${subdir}RegressionTest/regressionconfig.json"
     if [ -e "$regressionFile" ]; then
-        python3 -u RegressionTestRunner.py "${regressionFile}" || exit 1
+        python3 -u RegressionTestRunner.py "${regressionFile}" || regression_error=1
     fi 
 done
+
+if [ $regression_error -eq 1 ]; then
+  echo "One or more regression tests failed."
+  exit 1
+fi
