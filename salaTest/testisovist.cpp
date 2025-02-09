6e60d60c9b32c94ef4d0c87ac352f0567162dde2
Petros Koutsolampros <commits@orange-vertex.net>
2025-02-09 18:53:27 +0200

    Add more diagnostic prints

--- salaTest/testisovist.cpp
+++ salaTest/testisovist.cpp
@@ -62,27 +62,47 @@ TEST_CASE("Simple Isovist") {
 
     REQUIRE(isovist.points.size() == 8);
 
-    REQUIRE(isovist.points[0].x == Catch::Approx(3.0).epsilon(epsilon));
-    REQUIRE(isovist.points[0].y == Catch::Approx(3.0).epsilon(epsilon));
+    size_t i = 0;
+    REQUIRE(isovist.points[i].x == Catch::Approx(3.0).epsilon(epsilon));
+    REQUIRE(isovist.points[i].y == Catch::Approx(3.0).epsilon(epsilon));
 
-    REQUIRE(isovist.points[1].x == Catch::Approx(2.0).epsilon(epsilon));
-    REQUIRE(isovist.points[1].y == Catch::Approx(3.0).epsilon(epsilon));
+    i++;
+    REQUIRE(isovist.points[i].x == Catch::Approx(2.0).epsilon(epsilon));
+    REQUIRE(isovist.points[i].y == Catch::Approx(3.0).epsilon(epsilon));
 
-    REQUIRE(isovist.points[2].x == Catch::Approx(1.0).epsilon(epsilon));
-    REQUIRE(isovist.points[2].y == Catch::Approx(3.0).epsilon(epsilon));
+    i++;
+    REQUIRE(isovist.points[i].x == Catch::Approx(1.0).epsilon(epsilon));
+    REQUIRE(isovist.points[i].y == Catch::Approx(3.0).epsilon(epsilon));
 
-    REQUIRE(isovist.points[3].x == Catch::Approx(1.0).epsilon(epsilon));
-    REQUIRE(isovist.points[3].y == Catch::Approx(1.0).epsilon(epsilon));
+    i++;
+    REQUIRE(isovist.points[i].x == Catch::Approx(1.0).epsilon(epsilon));
+    REQUIRE(isovist.points[i].y == Catch::Approx(1.0).epsilon(epsilon));
 
-    REQUIRE(isovist.points[4].x == Catch::Approx(2.0).epsilon(epsilon));
-    REQUIRE(isovist.points[4].y == Catch::Approx(2.0).epsilon(epsilon));
+    i++;
+    REQUIRE(isovist.points[i].x == Catch::Approx(2.0).epsilon(epsilon));
+    REQUIRE(isovist.points[i].y == Catch::Approx(2.0).epsilon(epsilon));
 
-    REQUIRE(isovist.points[5].x == Catch::Approx(2.0).epsilon(epsilon));
-    REQUIRE(isovist.points[5].y == Catch::Approx(2.0).epsilon(epsilon));
+    i++;
+    REQUIRE(isovist.points[i].x == Catch::Approx(2.0).epsilon(epsilon));
+    REQUIRE(isovist.points[i].y == Catch::Approx(2.0).epsilon(epsilon));
 
-    REQUIRE(isovist.points[6].x == Catch::Approx(3.0).epsilon(epsilon));
-    REQUIRE(isovist.points[6].y == Catch::Approx(2.0).epsilon(epsilon));
+    //    i++;
+    //    REQUIRE(isovist.points[i].x == Catch::Approx(1.0).epsilon(epsilon));
+    //    REQUIRE(isovist.points[i].y == Catch::Approx(1.0).epsilon(epsilon));
 
-    REQUIRE(isovist.points[7].x == Catch::Approx(3.0).epsilon(epsilon));
-    REQUIRE(isovist.points[7].y == Catch::Approx(2.5).epsilon(epsilon));
+    //    i++;
+    //    REQUIRE(isovist.points[i].x == Catch::Approx(1.0).epsilon(epsilon));
+    //    REQUIRE(isovist.points[i].y == Catch::Approx(1.0).epsilon(epsilon));
+
+    //    i++;
+    //    REQUIRE(isovist.points[i].x == Catch::Approx(2.0).epsilon(epsilon));
+    //    REQUIRE(isovist.points[i].y == Catch::Approx(2.0).epsilon(epsilon));
+
+    i++;
+    REQUIRE(isovist.points[i].x == Catch::Approx(3.0).epsilon(epsilon));
+    REQUIRE(isovist.points[i].y == Catch::Approx(2.0).epsilon(epsilon));
+
+    i++;
+    REQUIRE(isovist.points[i].x == Catch::Approx(3.0).epsilon(epsilon));
+    REQUIRE(isovist.points[i].y == Catch::Approx(2.5).epsilon(epsilon));
 }
