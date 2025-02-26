1c6a2fa9ccab7c711938abd090a00bf32adbed3e
Petros Koutsolampros <commits@orange-vertex.net>
2025-02-26 16:57:55 +0200

    Inform the user that a selfcleaningfile has not truly been removed

--- cliTest/selfcleaningfile.hpp
+++ cliTest/selfcleaningfile.hpp
@@ -4,13 +4,20 @@
 
 #pragma once
 
+#include <iostream>
 #include <stdio.h>
 #include <string>
 
 class SelfCleaningFile {
   public:
     SelfCleaningFile(const std::string &filename) : m_filename(filename) {}
-    ~SelfCleaningFile() { std::remove(m_filename.c_str()); }
+    ~SelfCleaningFile() {
+        const int result = std::remove(m_filename.c_str());
+        if (result != 0) {
+            std::cerr << "failed to remove file: " << m_filename
+                      << std::endl; // No such file or directory
+        }
+    }
 
     const std::string &Filename() { return m_filename; }
 
