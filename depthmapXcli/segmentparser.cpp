ae7b93baccb0bc3675fd8621bcf3b9be29d26dcf
Petros Koutsolampros <commits@orange-vertex.net>
2025-03-03 16:04:17 +0200

    Fix tulip analysis

--- depthmapXcli/segmentparser.cpp
+++ depthmapXcli/segmentparser.cpp
@@ -168,7 +168,7 @@ void SegmentParser::run(const CommandLineParser &clp, IPerformanceSink &perfWrit
                  metaGraph.analyseSegmentsTulip(
                      dm_runmethods::getCommunicator(clp).get(), options.radiusSet, options.selOnly,
                      options.tulipBins, options.weightedMeasureCol, options.radiusType,
-                     options.choice,
+                     options.choice, options.weightedMeasureCol2, options.routeweightCol,
                      (mimicVersion.has_value() && mimicVersion == "depthmapX 0.8.0")))
         break;
     }
